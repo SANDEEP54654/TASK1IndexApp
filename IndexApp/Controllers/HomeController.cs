0ed5fb1dcc175665d18263652d6ed686289058db
SANDEEP54654 <sandeep54654@gmail.com>
2021-12-14 02:02:24 +0530

    All changes

---
+++ IndexApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using IndexApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndexApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(DateTime dateTime, int Number)
+        {
+            var date = dateTime == DateTime.MinValue ? DateTime.Now.AddDays(Number) : dateTime.AddDays(Number);
+            ViewBag.Date = dateTime == DateTime.MinValue ? DateTime.Now.ToString("dd/MM/yyyy") : dateTime.ToString("dd/MM/yyyy");
+            ViewBag.Number = Number.ToString();
+            ViewBag.Resullt = date.ToString("dd/MM/yyyy");
+            return View();
+        }
+    }
+}
